5b12aa06a5a2ee2a61cd57d449ed6fbe3e5c27f9
kavros <akavroulakis@gmail.com>
2018-11-17 18:31:53 +0000

    multiple changes
    fix serial case issues
    refactor code
    add timers
    wip for cmdline parser

--- src/imagenew_v2.c
+++ src/imagenew_v2.c
@@ -1,19 +1,20 @@
-/*
- * A simple serial solution to the Case Study exercise from the MPP
- * course.  Note that this uses the alternative boundary conditions
- * that are appropriate for the assessed coursework.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include "../include/arralloc.h"
 #include "../include/pgmio.h"
+#include "../include/argtable3.h"
 #include <mpi.h>
 #include <stdbool.h> 
 #include <assert.h>
 #define MAXITER   1500
 #define PRINTFREQ  200
+
+//#define IGNORE_PRINTF
+#ifdef IGNORE_PRINTF
+#define printf(fmt, ...) 
+#endif
+
 
 typedef struct topology
 {
@@ -31,7 +32,7 @@ double boundaryval(int i, int m);
 void imageRecontruction(topology topo,double** edge,double** buf,double** old,double** new,int N,int* dims);
 bool isNumberPrime(int num);
 void initialization(topology* topo,int worldSize,int M,int N,MPI_Comm* comm2d,int* dims);
-void scatter(double** masterbuf,double** buf,topology topo,int worldSize,char* input,MPI_Comm comm2d,int M,int N);
+void scatter(double** masterbuf,double** buf,topology topo,int worldSize,MPI_Comm comm2d);
 void gather(topology topo,double** masterbuf,double** buf,int M,int N,MPI_Comm comm2d,char* output,int worldSize);
 void createDataTypes(topology topo,int N);
 void computeBoundaryConditions(topology topo,int *dims,double **old,int N);
@@ -39,19 +40,34 @@ void halloSwapsHorizontal(double** old,topology topo);
 void halloSwapsVertical(double** old,topology topo);
 void printAverages(int iter,double **old,topology topo,int targetIter );
 bool isTheLastIteration(topology topo,double maxDelta,int iter);
+void loadImage(topology topo,double** masterbuf,char* input,int M,int N);
+void allocations(topology topo,int M,int N,double ***masterbuf,double ***buf,double ***old,double ***new, double ***edge);
+void deallocations(double **masterbuf,double **buf,double **old,double **new, double **edge);
+void cmdLineParser(int argc, char *argv[]);
 
 MPI_Datatype vectorMpxNp;    
-MPI_Datatype masterbufType;    
+MPI_Datatype vectorMpxNP_N;    
 MPI_Datatype vectorMpx1;
 
-int main (void)
+
+struct arg_lit *help;
+struct arg_int *delta, *targetIter;
+struct arg_file *input, *output;
+struct arg_end *end;
+
+int main (int argc, char *argv[])
 {
-    
+    cmdLineParser(argc,argv);
+
     MPI_Comm comm2d;
     int worldSize;
     topology topo;
     int M,N;
     char *input,*output;
+    double **old,**new,**edge,**buf,**masterbuf;
+    int dims[2];
+    double start,end;
+    
     input = "./data/input/edgenew192x128.pgm";
     output="./data/output/imagenew192x128.pgm";
     
@@ -59,43 +75,119 @@ int main (void)
     MPI_Comm_size(MPI_COMM_WORLD,&worldSize);
     
     pgmsize(input,&M,&N);
-    int dims[2];
-    dims[0]=0;dims[1]=0;
     
+    dims[0]=0;dims[1]=0;    
     initialization(&topo,worldSize,M,N,&comm2d,dims);
     //printf("p%d left=%d,right=%d up=%d down=%d \n",topo.rank,topo.left,topo.right,topo.up,topo.down);
     //printf("p%d Mp=%d, Np=%d\n",topo.rank,topo.Mp,topo.Np);
     //printf("M=%d,N=%d\n",M,N);
-    //printf("p%d (%d,%d)\n",topo.rank,topo.coords[0],topo.coords[1]);
-    
-    double **old,**new,**edge;
-    double **buf,**masterbuf;
-    masterbuf = (double**) arralloc(sizeof(double),2,M,N);
-    old = (double**) arralloc(sizeof(double),2,topo.Mp+2,topo.Np+2);
-    new = (double**) arralloc(sizeof(double),2,topo.Mp+2,topo.Np+2);
-    edge = (double**) arralloc(sizeof(double),2,topo.Mp+2,topo.Np+2);
-    buf = (double**) arralloc(sizeof(double),2,topo.Mp,topo.Np);
-    
+    printf("p%d (%d,%d)\n",topo.rank,topo.coords[0],topo.coords[1]);
 
-
+   
+    allocations( topo, M, N,&masterbuf,&buf,&old,&new,&edge);
     
     createDataTypes(topo, N);
     
-    scatter( masterbuf, buf, topo, worldSize, input, comm2d,M, N);
+    loadImage(topo,masterbuf,input, M, N);
+    
+    start = MPI_Wtime();
+    scatter( masterbuf, buf, topo, worldSize, comm2d);
     
     imageRecontruction( topo, edge, buf, old, new,N,dims);
     
     gather( topo, masterbuf,buf, M, N, comm2d,output,worldSize);
+    end = MPI_Wtime();
+    if(topo.rank == 0)
+        fprintf(stdout,"total time is %f(sec), woldSize=%d \n",end-start,worldSize);
     
     MPI_Finalize();
     
+    deallocations(masterbuf,old,new,edge,buf);
+
+    return 0;
+} 
+
+void cmdLineParser(int argc, char *argv[])
+{
+    void *argtable[] = {
+        help    = arg_litn("h", "help", 0, 1, "display this help and exit"),
+        delta   = arg_intn("d","delta","0 or 1",0,1,"activates delta"),
+        targetIter = arg_intn("t","target"," must be less than 1500",0,1,"select when to print average value"),
+        input   = arg_filen("i", NULL, "<file>", 0, 100, "input file"),
+        output  = arg_filen("o", NULL, "<file>", 0, 100, "output file"),
+        end     = arg_end(20),
+    };
+     
+
+    char progname[] = "coursework";
+    
+    int nerrors;
+    nerrors = arg_parse(argc,argv,argtable);
+    
+    /* special case: '--help' takes precedence over error reporting */
+    if (help->count > 0)
+    {
+        printf("Usage: %s", progname);
+        arg_print_syntax(stdout, argtable, "\n");
+        printf("Demonstrate command-line parsing in argtable3.\n\n");
+        arg_print_glossary(stdout, argtable, "  %-25s %s\n");
+
+        goto exit;
+    }
+
+    /* If the parser returned any errors then display them and exit */
+    if (nerrors > 0)
+    {
+        /* Display the error details contained in the arg_end struct.*/
+        arg_print_errors(stdout, end, progname);
+        printf("Try '%s --help' for more information.\n", progname);
+
+        goto exit;
+    }
+    if(input->count > 0 )
+    {
+        printf("input= %s\n",input->filename[0]);
+    }
+    if (output->count > 0 )
+    {
+        printf("output= %s\n",output->filename[0]);
+    }
+    if (targetIter->count > 0 )
+    {
+        printf("targetIter= %d\n",*(targetIter->ival));
+    }
+    if(delta->count > 0 )
+    {
+        printf("delta= %d\n",*(delta->ival));
+    }
+    
+    
+exit:
+    /* deallocate each non-null entry in argtable[] */
+    arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+    return;
+    
+}
+
+
+void allocations(topology topo,int M,int N,double ***masterbuf,double ***buf,double ***old,double ***new, double ***edge)
+{
+    *masterbuf = (double**) arralloc(sizeof(double),2,M,N);
+    *old = (double**) arralloc(sizeof(double),2,topo.Mp+2,topo.Np+2);
+    *new = (double**) arralloc(sizeof(double),2,topo.Mp+2,topo.Np+2);
+    *edge = (double**) arralloc(sizeof(double),2,topo.Mp+2,topo.Np+2);
+    *buf = (double**) arralloc(sizeof(double),2,topo.Mp,topo.Np);
+    
+}
+
+void deallocations(double **masterbuf,double **buf,double **old,double **new, double **edge)
+{
     free(masterbuf);
     free(old);
     free(new);
     free(edge);
     free(buf);
-    return 0;
-} 
+}
 
 void createDataTypes(topology topo,int N)
 {
@@ -109,8 +201,8 @@ void createDataTypes(topology topo,int N)
     int count_1 =topo.Mp;          
     int blocklength_1=topo.Np;
     int stride_1 = N;
-    MPI_Type_vector(count_1, blocklength_1, stride_1, MPI_DOUBLE, &masterbufType);
-    MPI_Type_commit(&masterbufType);
+    MPI_Type_vector(count_1, blocklength_1, stride_1, MPI_DOUBLE, &vectorMpxNP_N);
+    MPI_Type_commit(&vectorMpxNP_N);
     
     
     count = topo.Mp;
@@ -148,7 +240,7 @@ void gather(topology topo,double** masterbuf,double** buf,int M,int N,MPI_Comm c
             int startX = coords[0]*topo.Mp;
             //printf("p %d, coords[0]=%d,coords[1]=%d ,startY=%d, startX=%d\n",i,coords[0],coords[1],startY,startX);                             
             
-            MPI_Irecv(&masterbuf[startX][startY],1,masterbufType,i,0,MPI_COMM_WORLD,&request);
+            MPI_Irecv(&masterbuf[startX][startY],1,vectorMpxNP_N,i,0,MPI_COMM_WORLD,&request);
             MPI_Wait(&request,&status);
         }
         
@@ -161,23 +253,15 @@ void gather(topology topo,double** masterbuf,double** buf,int M,int N,MPI_Comm c
 }
 
 
-void scatter(double** masterbuf,double** buf,topology topo,int worldSize,char* input,MPI_Comm comm2d,int M,int N)
+void scatter(double** masterbuf,double** buf,topology topo,int worldSize,MPI_Comm comm2d)
 {
-    int i,j;
     MPI_Request request;
     MPI_Status status;
     if(topo.rank == 0)
     {
-        //printf("---master\n");
-        //printf("Processing %d x %d image\n", M, N);
-        //printf("Number of iterations = %d\n", MAXITER);
-        
-        printf("\nReading <%s>\n", input);
-        pgmread(input, &masterbuf[0][0], M, N);
-        
-        
         int coords[2];
-        
+        int i,j;
+
         for(i=1; i < worldSize; i++)
         {
             MPI_Cart_coords(comm2d, i, 2, coords);
@@ -185,7 +269,7 @@ void scatter(double** masterbuf,double** buf,topology topo,int worldSize,char* i
             int startX = coords[0]*topo.Mp;
             //printf("p %d, startY=%d, startX=%d\n",i,startY,startX);
             
-            MPI_Isend(&masterbuf[ startX][startY], 1, masterbufType, i, 0, MPI_COMM_WORLD,&request);
+            MPI_Isend(&masterbuf[ startX][startY], 1, vectorMpxNP_N, i, 0, MPI_COMM_WORLD,&request);
             
             MPI_Wait(&request,&status);
         }
@@ -194,8 +278,7 @@ void scatter(double** masterbuf,double** buf,topology topo,int worldSize,char* i
                 buf[i][j] = masterbuf[i][j];
     }
     
-    
-    
+       
     if(topo.rank !=0)
     {
         MPI_Irecv(&buf[0][0], 1, vectorMpxNp, 0, 0, MPI_COMM_WORLD, &request); 
@@ -204,7 +287,17 @@ void scatter(double** masterbuf,double** buf,topology topo,int worldSize,char* i
     }
     
 }
-
+void loadImage(topology topo,double** masterbuf,char* input,int M,int N)
+{
+    if(topo.rank == 0)
+    {
+        //printf("---master\n");
+        //printf("Processing %d x %d image\n", M, N);
+        //printf("Number of iterations = %d\n", MAXITER);
+        printf("\nReading <%s>\n", input);
+        pgmread(input, &masterbuf[0][0], M, N);
+    }
+}
 bool isNumberPrime(int num)
 {
     assert(num > 0);
@@ -236,16 +329,31 @@ void initialization(topology* topo,int worldSize,int M,int N,MPI_Comm* comm2d,in
 
     comm  = MPI_COMM_WORLD;          
     reorder = false;
-    disp = 1;                   
-    if( isNumberPrime(worldSize) == true )
+    disp = 1;  
+    if(worldSize == 1)
+    {
+        topo->Mp = M;
+        topo->Np = N;
+        dims[0] = 0;
+        dims[1] = 0;
+        period[0] =  false;
+        period[1] =  false;
+        
+        MPI_Dims_create(worldSize,ndims,dims);
+        MPI_Cart_create(comm,ndims,dims,period,reorder,comm2d);
+        MPI_Cart_shift(*comm2d,0,disp,&(topo->left),&(topo->right));
+        MPI_Cart_shift(*comm2d,1,disp,&(topo->down),&(topo->up));
+        MPI_Comm_rank(*comm2d,&(topo->rank));
+    }
+    else if( isNumberPrime(worldSize) == true )
     {   
         //finds a 1D decomposition
         
-        printf("1D\n");
+        //printf("1D\n");
         if( (M % worldSize == 0) )
         {
             //vertical decomposition            
-            printf("vertical\n");
+            //printf("vertical\n");
             topo->Mp = M/worldSize;
             topo->Np = N;
             dims[0] = 0;
@@ -255,7 +363,7 @@ void initialization(topology* topo,int worldSize,int M,int N,MPI_Comm* comm2d,in
         }
         else if( (N % worldSize == 0) )
         {
-            printf("horizontal\n");
+            //printf("horizontal\n");
             //horizontal decomposition
             topo->Mp = M;
             topo->Np = N/worldSize;
@@ -266,7 +374,7 @@ void initialization(topology* topo,int worldSize,int M,int N,MPI_Comm* comm2d,in
         }
         else
         {
-            printf("Combination of image and thread number is not supported.\n");
+            //printf("Combination of image and thread number is not supported.\n");
             MPI_Finalize();
             exit(1);
         }
@@ -293,7 +401,7 @@ void initialization(topology* topo,int worldSize,int M,int N,MPI_Comm* comm2d,in
         MPI_Cart_shift(*comm2d,0,disp,&(topo->left),&(topo->right));
         MPI_Cart_shift(*comm2d,1,disp,&(topo->down),&(topo->up));
         MPI_Comm_rank(*comm2d,&(topo->rank));
-        printf("2d (%dx%d) \n",dims[0],dims[1]);
+        //printf("2d (%dx%d) \n",dims[0],dims[1]);
         bool isTopologyDivisible = ( (N%dims[0]) == 0) && ( (M % dims[1]) ==0 );
         if(  isTopologyDivisible   )
         {
@@ -302,7 +410,7 @@ void initialization(topology* topo,int worldSize,int M,int N,MPI_Comm* comm2d,in
         }
         else
         {
-            printf("Combination of image and thread number is not supported.\n");
+            //printf("Combination of image and thread number is not supported.\n");
             MPI_Finalize();
             exit(1);
         } 
