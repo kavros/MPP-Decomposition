30338598ab353257b9eb130ee047e78f01036b2c
kavros <akavroulakis@gmail.com>
2018-11-19 16:41:52 +0000

    multiple chages on scripts
    qsub script run every experiment for 10 times
    add image as an inpu as  parameter in order make measurements with (small,medium and large images)

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,6 @@
 
 for i in {1,2} #,2,3,4,8}
 do
-	qsub -v MPISIZE=$i -o "./data/qsub/$i.txt"  scripts/image.pbs 
+	qsub -v MPISIZE=$i,IMG="./data/input/edgenew768x768.pgm" -o "./data/qsub/$i.txt"  scripts/image.pbs  	
+
 done
