7b164572940d315703dabf0aa2620bf9a8486e6f
kavros <akavroulakis@gmail.com>
2018-11-19 22:55:32 +0000

    fix script to use the new cmd line args

--- scripts/validateOutput.sh
+++ scripts/validateOutput.sh
@@ -4,14 +4,14 @@
 # and compare the output images with the image from the serial execution.(delta is disabled)
 for i in {1,2,3,4,8}
 do
-	mpirun -n $i ./build/image -o "./data/output/imagenew192x128_$i.pgm" 
+	mpirun -n $i ./build/image -e "./data/output/imagenew192x128_$i.pgm" 
 	diff  "data/output/imagenew192x128_$i.pgm" data/output/imagenew192x128_expectedOutput.pgm
 done
 
-mpirun -n 1 ./build/image -d 1 -o ./data/output/imagenew192x128_d_1.pgm 
+mpirun -n 1 ./build/image -d 1 -e ./data/output/imagenew192x128_d_1.pgm 
 for i in {2,3,4,8}
 do
-	mpirun -n $i ./build/image -d 1 -o "./data/output/imagenew192x128_d_$i.pgm" 
+	mpirun -n $i ./build/image -d 1 -e "./data/output/imagenew192x128_d_$i.pgm" 
 	diff ./data/output/imagenew192x128_d_1.pgm  "./data/output/imagenew192x128_d_$i.pgm" 
 done
 
