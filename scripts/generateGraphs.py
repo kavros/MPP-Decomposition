2a77892765606594b07301c019d41f0aea780d5e
kavros <akavroulakis@gmail.com>
2018-11-19 18:13:25 +0000

    add scripts for runing on qsub and automatic graphs generation

---
+++ scripts/generateGraphs.py
@@ -0,0 +1,82 @@
+import os
+import operator
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+small_np_to_time=defaultdict(list)
+medium_np_to_time=defaultdict(list)
+large_np_to_time=defaultdict(list)
+
+def GetAxis(dictionary):
+	xAxisContent=[]
+	yAxisContent=[]
+	for key in sorted(dictionary.iterkeys()):
+		xAxisContent.append(key)
+		yAxisContent.append(dictionary[key])
+	return xAxisContent,yAxisContent
+
+
+
+def GenerateLineChart(dict1,dict2,dict3,imageName1,imageName2,imageName3,fileName):
+	x1,y1 = GetAxis(dict1)
+	x2,y2 = GetAxis(dict2)
+	x3,y3 = GetAxis(dict3)
+
+	fig, ax = plt.subplots()
+	plt.plot(x1,y1,marker="x")
+	plt.plot(x2,y2,marker="x")
+	plt.plot(x3,y3,marker="x")
+	ax.set(xlabel="number of processes", ylabel="total time in secs")	
+	ax.grid()
+	plt.legend([imageName1, imageName2,imageName3], loc=1)
+	plt.show()
+	#path="../results/graphs/"
+	#plt.savefig((path+fileName), format='eps', dpi=1000)
+
+
+
+def initDictionary(dictionary, path):
+	print path
+	# read all files and create a hashtable 
+	# with key equals  to the number of threads
+	# and values equals to list with the times
+	for fileName in os.listdir(path):
+		if(".txt" not in fileName):
+			continue
+		f = open(path+fileName,"r")
+		lines =  f.readlines();
+		for line in lines:
+			if("total time" in line):
+				time = float((line.split(" ")[3]).strip())
+				np = int(line.split(" ")[7].strip())
+				dictionary[np].append(time)
+
+	# sort  time measurments
+	for key in dictionary.keys():
+		dictionary[key] = sorted(dictionary[key])
+	print dictionary
+	# update hash table value for each key with median time instead of list of times
+	for key in dictionary.keys():
+		medianTime = (dictionary[key])[len(dictionary[key])/2]
+		dictionary[key] =  medianTime
+
+
+
+def main():
+	
+	initDictionary(small_np_to_time,"./data/qsub/small/")
+	initDictionary(medium_np_to_time,"./data/qsub/medium/")
+	initDictionary(large_np_to_time,"./data/qsub/large/")
+	print small_np_to_time
+	print medium_np_to_time
+	print large_np_to_time
+	GenerateLineChart(small_np_to_time,medium_np_to_time,large_np_to_time,"192x128","512x384","768x768","test.eps")
+
+if __name__ == "__main__":
+	main()
+
+
+
+
+
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 
-for i in {1,2} #,2,3,4,8}
+for i in {1,2,3,4,8}
 do
-	qsub -v MPISIZE=$i,IMG="./data/input/edgenew768x768.pgm" -o "./data/qsub/$i.txt"  scripts/image.pbs  	
-
+	qsub -v MPISIZE=$i,INPUT_IMG="./data/input/edgenew192x128.pgm",OUTPUT_IMG="./data/qsub/small/imagenew192x128_$i.pgm" -o "./data/qsub/small/$i.txt"  scripts/image.pbs  	
+	qsub -v MPISIZE=$i,INPUT_IMG="./data/input/edgenew512x384.pgm",OUTPUT_IMG="./data/qsub/medium/imagenew512x384_$i.pgm" -o "./data/qsub/medium/$i.txt"  scripts/image.pbs  	
+	qsub -v MPISIZE=$i,INPUT_IMG="./data/input/edgenew768x768.pgm",OUTPUT_IMG="./data/qsub/large/imagenew768x768_$i.pgm" -o "./data/qsub/large/$i.txt"  scripts/image.pbs  	
 done
+
