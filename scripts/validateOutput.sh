481043b4f3ee7356c66a032e7b8e75d149d6595f
kavros <akavroulakis@gmail.com>
2018-11-20 22:31:01 +0000

    add more processes for testing

--- scripts/validateOutput.sh
+++ scripts/validateOutput.sh
@@ -2,14 +2,14 @@
 
 # run the application using various number of processes
 # and compare the output images with the image from the serial execution.(delta is disabled)
-for i in {1,2,3,4,8}
+for i in {1,2,3,4,8,16,24,32,64}
 do
 	mpirun -n $i ./build/image -e "./data/output/imagenew192x128_$i.pgm" 
 	diff  "data/output/imagenew192x128_$i.pgm" data/output/imagenew192x128_expectedOutput.pgm
 done
 
 mpirun -n 1 ./build/image -d 1 -e ./data/output/imagenew192x128_d_1.pgm 
-for i in {2,3,4,8}
+for i in {1,2,3,4,8,16,24,32,64}
 do
 	mpirun -n $i ./build/image -d 1 -e "./data/output/imagenew192x128_d_$i.pgm" 
 	diff ./data/output/imagenew192x128_d_1.pgm  "./data/output/imagenew192x128_d_$i.pgm" 
