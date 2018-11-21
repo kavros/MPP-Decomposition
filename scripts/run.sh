3357404c09470419d14f837c3171963b1546977e
kavros <akavroulakis@gmail.com>
2018-11-21 00:12:03 +0000

    fix problem with 64 processes

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 
-#for i in {1,2,3,4,8}
-for i in {1,2,3,4,8,16,24,32,64}
+for i in {1,2,3,4,8,16,32}
 do
 	qsub -v MPISIZE=$i,INPUT_IMG="./data/input/edgenew192x128.pgm",OUTPUT_IMG="./data/qsub/small/imagenew192x128_$i.pgm" -o "./data/qsub/small/$i.txt"  scripts/image.pbs  	
 	qsub -v MPISIZE=$i,INPUT_IMG="./data/input/edgenew512x384.pgm",OUTPUT_IMG="./data/qsub/medium/imagenew512x384_$i.pgm" -o "./data/qsub/medium/$i.txt"  scripts/image.pbs  	
