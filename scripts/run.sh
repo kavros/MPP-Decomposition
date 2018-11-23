908702a9df010bfe7217367611324625d4a4712a
kavros <akavroulakis@gmail.com>
2018-11-23 22:00:00 +0000

    fix issue with 3 processes

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,19 +4,11 @@ nodes=1
 for i in {1,2,3,4,8,16,32,64}
 do
 	
-	if [ "$i" -eq "2" ]
+	if [ "$i" -lt "5" ]
 	then
-		nodes=2
+		nodes="$i"
 		
 	fi
-
-	if [ "$i" -eq "4" ]
-	then
-		nodes=4
-		
-	fi
-	
-
 	
 	qsub  -l select=$nodes:ncpus=36 -v NPROC=$i,INPUT_IMG="./data/input/edgenew192x128.pgm",OUTPUT_IMG="./data/qsub/small/imagenew192x128_$i.pgm" -o "./data/qsub/small/$i.txt"  scripts/image.pbs  	
 	qsub  -l select=$nodes:ncpus=36 -v NPROC=$i,INPUT_IMG="./data/input/edgenew512x384.pgm",OUTPUT_IMG="./data/qsub/medium/imagenew512x384_$i.pgm" -o "./data/qsub/medium/$i.txt"  scripts/image.pbs  	
